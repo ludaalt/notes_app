15820e194fba8315bb4d3e5652a7d8020cdab948
Людмила Алтухова <luda.tamb@mail.ru>
2023-03-03 16:53:35 +0300

    refactor: add basic data

---
+++ src/data/notes.ts
@@ -0,0 +1,15 @@
+export const notes = [
+  {
+    id: 1,
+    title: "note 1",
+    description: "",
+    date: "",
+  },
+
+  {
+    id: 2,
+    title: "note 2",
+    description: "",
+    date: "",
+  },
+];
