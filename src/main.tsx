bf823873290c3dee3e2c3a194657bd3cfdcf4167
Людмила Алтухова <luda.tamb@mail.ru>
2023-03-03 16:44:40 +0300

    refactor: add global styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,24 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { createGlobalStyle } from "styled-components";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  ul {
+    list-style: none;
+  }
+`;
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
+    <GlobalStyle />
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
